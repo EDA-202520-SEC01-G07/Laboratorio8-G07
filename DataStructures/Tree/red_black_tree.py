a4580e6de0de3f68354abed7c77e75233247af67
Juliana-_Rod-1234 <js.rodriguezm1234@uniandes.edu.co>
2025-10-25 12:57:00 -0500

    get y put

--- DataStructures/Tree/red_black_tree.py
+++ DataStructures/Tree/red_black_tree.py
@@ -1,3 +1,5 @@
+from DataStructures.Tree import rbt_node as n
+
 def new_map():
     return {"root":None}
 def default_compare(key, element):
@@ -7,6 +9,19 @@ def default_compare(key, element):
         return 1
     else:
         return 0
+    
+def size_tree(root):
+    if root is not None:
+        return 1 + size_tree(root["left"]) + size_tree(root["right"])
+    else:
+        return 0
+    
+def is_red(node):
+    if node is not None and node["color"] == 0:
+        return True
+    else:
+        return False
+    
 def rotate_left(node): #Cuando hay un enlace rojo en la derecha
     rotar = node["right"]
     temporal = rotar["left"]
@@ -16,7 +31,7 @@ def rotate_left(node): #Cuando hay un enlace rojo en la derecha
     rotar["color"]=1 #Black
     return rotar
 
-def rotate_right(node):
+def rotate_right(node): #Cuando hay dos enlaces rojos seguidos
     if node or node["left"] is None:
         return None
     else:
@@ -26,20 +41,66 @@ def rotate_right(node):
         left["right"]["color"] = 0
     return left 
 
-def size_tree(root):
-    if root is not None:
-        return 1 + size_tree(root["left"]) + size_tree(root["right"])
-    else:
-        return 0
-def is_red(node):
-    if node is not None and node["color"] == 0:
-        return True
-    else:
-        return False
-def flip_node_color(node):
+def flip_node_color(node): #Cambia el color de un solo nodo
     if node is not None:
-        if node ["left"] ["color"]== 0:
-            node ["left"] ["color"]= 1
-        if node ["right"] ["color"]== 1:
-            node ["right"] ["color"]= 0
+        color = node["color"]
+        if color == 0:
+            node["color"] = 1
+        else:
+            node["color"] = 0
     return node
+
+def flip_colors(node): #Cambia el color del nodo y de sus dos hijos
+    if node is not None:
+        flip_node_color(node)
+        izq = node["left"]["color"]
+        der = node["right"]["color"]
+        if node["left"] is not None:
+            if izq == 0:
+                node["left"]["color"] = 1
+            else:
+                node["left"]["color"] = 0
+        if node["right"] is not None:
+            if der == 0:
+                node["right"]["color"] = 1
+            else:
+                node["right"]["color"] = 0
+    return node
+
+def put(rbt, key, value): #Ingresa una pareja llave,valor. Si la llave ya existe, se reemplaza el valor.
+    insert_node(rbt["root"], key, value)
+    return rbt
+
+def insert_node(root, key, value): #Ingresa una pareja llave,valor. Si la llave ya existe, se reemplaza el valor.
+    if root is None:
+        return n.new_node(key, value)
+    cmp = default_compare(key, root["key"])
+    if cmp == -1:
+        insert_node(root["left"], key, value)
+    elif cmp == 1:
+        insert_node(root["right"], key, value)
+    elif cmp == 0:
+        root["value"] = value
+        
+    if is_red(root["right"]) and not is_red(root["left"]): #Si hay un enlace rojo en la derecha
+        rotate_left(root)
+    if is_red(root["right"]) and is_red(root["left"]): #Si los hijos tienen enlace rojo
+        flip_colors(root)
+    if is_red(root["left"]) and is_red(root["left"]["left"]): #Si hay dos enlaces rojos a la izq seguidos
+        rotate_right(root)
+    return root 
+
+def get(rbt, key): #Devuelve el valor de la llave
+    return get_node(rbt["root"], key)
+
+def get_node(root, key):
+    if root == None:
+        return None
+    cmp = default_compare(key, root["key"])
+    if cmp == 0:
+        return root["value"]
+    else:
+        if cmp == -1:
+            return get_node(root["left"], key)
+        if cmp == 0:
+            return get_node(root["right"], key)
